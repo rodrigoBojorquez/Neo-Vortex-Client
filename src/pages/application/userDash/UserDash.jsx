d1db2dea235b444f6199faa82c6b20f4a4d20f0e
Alexis Dolores <alexisdanieldr@gmail.com>
2023-12-26 11:48:32 -0500

    Dash user design responsive

--- src/pages/application/userDash/UserDash.jsx
+++ src/pages/application/userDash/UserDash.jsx
@@ -7,6 +7,7 @@ import { FaUpload } from "react-icons/fa";
 import { FaRobot } from "react-icons/fa";
 import { FaDropbox } from "react-icons/fa";
 import Table from './components/Tabla_Docs';
+import doc from '../../../assets/img/doc.png'
 
 
 function UserDash() {
@@ -14,11 +15,18 @@ function UserDash() {
     { id: 1, name: 'Juan Pérez', age: 25, email: 'juan@example.com' },
     // Agrega más datos según sea necesario
   ];
+
+  const datosEjeDocs = [
+    { imagen: doc, titulo: 'Titulo del doc 1' },
+    { imagen: doc, titulo: 'Titulo del doc 2' },
+    { imagen: doc, titulo: 'Titulo del doc 3' },
+  ];
+
   return (
     <div>
       <Header />
-      <section className='w-full bg-[#4D4C7D] mt-11 h-[300px] flex justify-center items-center'>
-        <div className='grid grid-cols-4 gap-20 max-w-[1000px] mx-auto'>
+      <section className='w-full bg-[#4D4C7D] mt-11  flex justify-center items-center'>
+      <div className='grid grid-cols-2  lg:grid-cols-4 gap-8 md:gap-12 lg:gap-20 max-w-[1000px] mx-auto my-10'>
           <div className='w-[150px] h-[220px] rounded-lg mt-4 bg-[#363062] flex flex-col items-center'>
             <FaPlus size={120} color="white" className='rounded-md text-center pb-2 pt-10' />
             <h2 className='text-2xl font-semibold text-white text-center'>Nuevo</h2>
@@ -40,32 +48,32 @@ function UserDash() {
 
       <h1 className=' text-3xl font-semibold my-4 ml-4 '>Recientes</h1>
 
-      <section className='w-full h-[230px] mb-4 flex justify-center items-center'>
-        <div className='grid grid-cols-3 gap-20'>
-          <div className='w-[350px] h-[210px] rounded-lg mt-4 bg-[#363062] flex flex-col items-center'>
-            <div className='w-full h-[50px] rounded-lg bg-black text-center align-baseline '>
-              <h3 className='text-white mt-3 text-xl'>Titulo del doc</h3>
+      <section className='w-full mb-4 flex justify-center items-center'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20'>
+          {datosEjeDocs.map((doc, index) => (
+            <div
+              key={index}
+              className='w-[350px] h-[210px] rounded-lg mt-4 bg-[#363062] flex flex-col items-center'
+              style={{
+                backgroundImage: `url(${doc.imagen})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center center',
+              }}
+            >
+              <div className='w-full h-[50px] rounded-lg bg-black text-center align-end '>
+                <h3 className='text-white mt-3 text-xl'>{doc.titulo}</h3>
+              </div>
             </div>
-          </div>
-          <div className='w-[350px] h-[210px] rounded-lg mt-4 bg-[#363062] flex flex-col items-center'>
-            <div className='w-full h-[50px] rounded-lg bg-black text-center align-baseline '>
-              <h3 className='text-white mt-3 text-xl'>Titulo del doc</h3>
-            </div>
-          </div>
-          <div className='w-[350px] h-[210px] rounded-lg mt-4 bg-[#363062] flex flex-col items-center'>
-            <div className='w-full h-[50px] rounded-lg bg-black text-center align-baseline '>
-              <h3 className='text-white mt-3 text-xl'>Titulo del doc</h3>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
 
       <h1 className=' text-3xl font-semibold my-4 ml-4 '>Documentos</h1>
 
-      <Table  data={data} />
+      <Table data={data} />
 
       <Footer />
-    </div>
+    </div >
   )
 }
 
